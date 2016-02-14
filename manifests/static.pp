5b27ae8569915bac563d08bbd3a6dd971f1d01d3
Ugo Paternostro <ugo.git@paternostro.org>
2016-02-14 18:04:19 +0100

    Adding support for static userdb

---
+++ manifests/static.pp
@@ -0,0 +1,22 @@
+class dovecot::static (
+  $uid = 'vmail',
+  $gid = 'vmail',
+  $home = '/srv/vmail/%d/%u/',
+  $allow_all_users = undef,
+) {
+  file { "/etc/dovecot/conf.d/auth-static.conf.ext":
+    ensure  => file,
+    content => template('dovecot/auth-static.conf.ext'),
+    mode    => '0600',
+    owner   => root,
+    before  => Exec['dovecot'],
+  }
+
+  dovecot::config::dovecotcfmulti { 'staticauth':
+    config_file => 'conf.d/10-auth.conf',
+    changes     => [
+      "set include 'auth-static.conf.ext'",
+    ],
+    require     => File["/etc/dovecot/conf.d/auth-static.conf.ext"]
+  }
+}
